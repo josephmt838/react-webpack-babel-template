6fa2aa701cc2daaaa605af6bcfc3cdcc77dd47fe
Joseph Thompson (contractor) <joseph.x.thompson@macys.com>
2019-06-20 09:58:01 -0400

    added babel and react configs

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "index.scss";
+
+const App = () => {
+  return <section>App Component</section>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require("path");
+import path from "path";
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
@@ -19,5 +19,22 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html")
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules/",
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: ["file-loader"]
+      }
+    ]
+  }
 };
